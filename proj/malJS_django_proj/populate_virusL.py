4e8e2b22305a1634d97182d4af6df0526d265ee2
Luoai <abc5462@psu.edu>
2015-11-17 10:10:08 -0500

    1117

--- proj/malJS_django_proj/populate_virusL.py
+++ proj/malJS_django_proj/populate_virusL.py
@@ -4,21 +4,41 @@ import urllib2
 
 
 import os
-os.envron.setdefault('DJANGO_SETTINGS_MODULE','malJS_django_proj')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','malJS_django_proj')
 
 import django
 django.setup()
 
 from virusList.models import virusList
 
+
 def populate():
-	path=""
-	md5=""
-	rp=get_report_dict(md5)
-	add_virus(
-		MD5=md5,
-		size_KB=sizeOf(path),
-		VitusTotal_link=rp.permalink)
+	#from datetime import datetime
+	#generate file path day be day
+	'''
+	now=datetime.now();
+	day=str(now.day-1);
+	if (len(day)==1):
+		day='0'+day;
+	mon=str(now.month);
+	if (len(mon)==1):
+		mon='0'+mon;
+	dirName=str(now.year)+"-"+mon+"-"+day
+	'''
+
+	MAL_DIR = '/root/JS_repository/2015-11-14'
+	
+	dirs=os.listdir(MAL_DIR)
+	#send malicious html found by MALTRIEVE to VIRUSTOTAL to scan
+	for f in dirs:
+		path=MAL_DIR
+		md5=f
+		rp=get_report_dict(md5)
+		add_virus(
+			MD5=md5,
+			size_KB=sizeOf(path),
+			VitusTotal_link=rp.permalink)
+		break
 
 
 def add_virus(md5,size,url):
@@ -33,7 +53,7 @@ def sizeOf(PATH):
 def get_report_dict(self,resource):
 
     result_dict = {}
-    
+    APIKEY="659fd24c11e839f866f32b0dfa37887e91d6713439505e717541595252d3c47f"
     url = "https://www.virustotal.com/vtapi/v2/file/report"
     parameters = {"resource": resource,
                    "apikey": APIKEY}
@@ -41,6 +61,6 @@ def get_report_dict(self,resource):
     req = urllib2.Request(url, data)
     response = urllib2.urlopen(req)
     json = response.read()
-    
+    print json
     response_dict = simplejson.loads(json)
     return response_dict;
